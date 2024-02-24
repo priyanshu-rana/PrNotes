b5fb6d1e581e16721550ff2949fbb2daea03cda3
Priyanshu Rana <priyanshurana1234@gmail.com>
2024-02-24 22:37:20 +0530

    firebase-FE-configration-fixes

--- FE/src/firebase.ts
+++ FE/src/firebase.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
-import {} from "firebase/analytics";
 import { getStorage } from "firebase/storage";
-const env = process.env;
+
+const env = import.meta.env;
 const firebaseConfig = {
-  apiKey: env.API_KEY,
-  authDomain: env.AUTH_DOMAIN,
-  projectId: env.PROJECT_ID,
-  storageBucket: env.STORAGE_BUCKET,
-  messagingSenderId: env.MESSAGING_SENDER_ID,
-  appId: env.APP_ID,
-  measurementId: env.MEASUREMENT_ID,
+  apiKey: env.VITE_API_KEY,
+  authDomain: env.VITE_AUTH_DOMAIN,
+  projectId: env.VITE_PROJECT_ID,
+  storageBucket: env.VITE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_MESSAGING_SENDER_ID,
+  appId: env.VITE_APP_ID,
+  measurementId: env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
