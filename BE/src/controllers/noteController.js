f95e8c99b91759f95aa1249fd834e68d18b85de5
Priyanshu Rana <priyanshurana1234@gmail.com>
2024-02-26 23:46:38 +0530

    code-refactoring-&-redundant-code-cleanup

--- BE/src/controllers/noteController.js
+++ BE/src/controllers/noteController.js
@@ -2,65 +2,65 @@ const admin = require("firebase-admin");
 const noteModel = require("../models/noteModel");
 // const UUID = require("uuidv4");
 
-const createNote = async (req, res) => {
-  try {
-    const { title, description } = req.body;
-    const file = req.file;
-    // let uuid = UUID;
-    const bucket = admin.storage().bucket("gs://prnotes-7c669.appspot.com");
-    const fileName = `${Date.now()}-${file?.originalname}`;
-    const blob = bucket.file(fileName);
-    const blobStream = blob.createWriteStream();
+// const createNote = async (req, res) => {
+//   try {
+//     const { title, description } = req.body;
+//     const file = req.file;
+//     // let uuid = UUID;
+//     const bucket = admin.storage().bucket("gs://prnotes-7c669.appspot.com");
+//     const fileName = `${Date.now()}-${file?.originalname}`;
+//     const blob = bucket.file(fileName);
+//     const blobStream = blob.createWriteStream();
 
-    blobStream.on("error", (err) => {
-      console.log("Error while uploading image", err);
-      res.status(500).json({ error: "Image upload failed!" });
-    });
-    blobStream.on("finish", async () => {
-      const signedUrlConfig = {
-        action: "read",
-        // expires: Date.now() + 15 * 60 * 1000, // URL expires in 15 minutes
-        expires: Date.now() + 315532800, // URL expires in 10 years {approx}
-      };
-      // const imageResponse = await bucket.upload(
-      //   "gs://prnotes-7c669.appspot.com",
-      //   {
-      //     destination: `image/${file?.originalname}`,
-      //     resumable: true,
-      //     metadata: {
-      //       metadata: {
-      //         firebaseStorageDownloadTokens: uuid,
-      //       },
-      //     },
-      //   }
-      // );
+//     blobStream.on("error", (err) => {
+//       console.log("Error while uploading image", err);
+//       res.status(500).json({ error: "Image upload failed!" });
+//     });
+//     blobStream.on("finish", async () => {
+//       const signedUrlConfig = {
+//         action: "read",
+//         // expires: Date.now() + 15 * 60 * 1000, // URL expires in 15 minutes
+//         expires: Date.now() + 315532800, // URL expires in 10 years {approx}
+//       };
+//       // const imageResponse = await bucket.upload(
+//       //   "gs://prnotes-7c669.appspot.com",
+//       //   {
+//       //     destination: `image/${file?.originalname}`,
+//       //     resumable: true,
+//       //     metadata: {
+//       //       metadata: {
+//       //         firebaseStorageDownloadTokens: uuid,
+//       //       },
+//       //     },
+//       //   }
+//       // );
 
-      const [signedUrl] = await blob.getSignedUrl(signedUrlConfig);
-      // const publicUrl = `https://storage.googleapis.com/v0/b/${
-      //   bucket.name
-      // }${encodeURIComponent(imageResponse[0].name)}?alt=media&token=${uuid}`;
+//       const [signedUrl] = await blob.getSignedUrl(signedUrlConfig);
+//       // const publicUrl = `https://storage.googleapis.com/v0/b/${
+//       //   bucket.name
+//       // }${encodeURIComponent(imageResponse[0].name)}?alt=media&token=${uuid}`;
 
-      const newNote = new noteModel({
-        // new keyword
-        title: title,
-        description: description,
-        userId: req.userId,
-        image: file && signedUrl,
-      });
-      await newNote.save();
-      res
-        .status(201)
-        .json({ message: "Note created successfull", data: { newNote } });
-    });
-    blobStream.end(file?.buffer);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Note creation failed" });
-  }
-};
+//       const newNote = new noteModel({
+//         // new keyword
+//         title: title,
+//         description: description,
+//         userId: req.userId,
+//         image: file && signedUrl,
+//       });
+//       await newNote.save();
+//       res
+//         .status(201)
+//         .json({ message: "Note created successfull", data: { newNote } });
+//     });
+//     blobStream.end(file?.buffer);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Note creation failed" });
+//   }
+// };
 
 // create new api as we accept attachment url insted of attachment
-const modifiedCreateNote = async (req, res) => {
+const createNote = async (req, res) => {
   try {
     const { title, description, attachmentUrl } = req.body;
     const newNote = noteModel({
@@ -133,10 +133,6 @@ const deleteNote = async (req, res) => {
   const noteId = req.params.noteId; //noteId is from paramas --> line.no 14 noteRoutes
   try {
     const note = await noteModel.findByIdAndDelete(noteId);
-    // const imagePath = `./uploads/${note.image}`;
-    // fs.remove(imagePath)
-    //   .then(() => console.log("Image deleted sucessfully"))
-    //   .catch((e) => console.log(e));
     res.status(202).json(note);
   } catch (error) {
     console.log(error);
--- BE/src/routes/noteRoutes.js
+++ BE/src/routes/noteRoutes.js
@@ -1,39 +1,26 @@
 const express = require("express");
 const multer = require("multer");
 const auth = require("../middlewares/auth");
-// const admin = require("firebase-admin");
-// const firebaseConfig = require("../../firebase.conf");
-// const serviceAccount = require("../../");
-// firebase-adminsdk-mbuer@prnotes-7c669.iam.gserviceaccount.com
 
 const {
   createNote,
   getNotes,
   updateNote,
   deleteNote,
-  modifiedCreateNote,
 } = require("../controllers/noteController");
 
 const noteRouter = express.Router();
 const storage = multer.memoryStorage();
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(JSON.parse(firebaseConfig)),
-//   // credential: admin.credential.cert(credentials),
-
-//   storageBucket: "gs://prnotes-7c669.appspot.com",
-// });
-
+//TODO: Remove uplaod as we are not using multer now and modify Update API accordingly
 const upload = multer({
   storage: storage,
-  //  limits: { fileSize: 1000 }
 });
 const imageUpload = upload.single("image");
 
-// noteRouter.use("/image", express.static("./uploads"));
-noteRouter.post("/", auth, modifiedCreateNote);
+noteRouter.post("/", auth, createNote);
 noteRouter.get("/", auth, getNotes);
-noteRouter.put("/:noteId", auth, imageUpload, updateNote);
+noteRouter.put("/:noteId", auth, imageUpload, updateNote); // TODO: Modify Update Note API as per Attachment & Check when Updating attachement related prevously updated attachment must delete/remove from the firebase storage/bucket
 noteRouter.delete("/:noteId", auth, deleteNote);
 
 module.exports = noteRouter;
