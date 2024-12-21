2bc9ee5557c2080c37769872d6be5397ed320636
Priyanshu Rana <priyanshurana1234@gmail.com>
2024-12-21 22:55:45 +0530

    [MID:BE] forgot-password-api-&-nodemailer-integration

--- BE/src/controllers/userControllers.js
+++ BE/src/controllers/userControllers.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 const SECRET_KEY = process.env.SECRET_KEY;
+const RESET_SECRET_KEY = process.env.RESET_SECRET_KEY;
 
 const signUp = async (req, res) => {
   const { first_name, last_name, password, email } = req.body;
@@ -64,4 +66,52 @@ const signIn = async (req, res) => {
   }
 };
 
-module.exports = { signUp, signIn };
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const existingUser = await userModel.findOne({ email: email });
+    if (!existingUser) {
+      return res.status(404).json({
+        message: "Unable to find account with provided Email!",
+      });
+    }
+
+    // Generate JWT token with expiration
+    const resetToken = jwt.sign({ id: existingUser._id }, RESET_SECRET_KEY, {
+      expiresIn: "10m",
+    });
+
+    // Send email with reset link
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const resetLink = `${process.env.FE_URL}/reset-password?token=${resetToken}`;
+    try {
+      await transporter.sendMail({
+        to: email,
+        subject: "Password Reset",
+        html: `<p>You requested a password reset. Click <a href="${resetLink}">here</a> to reset you password.
+        The link expires in 10 minutes.
+        </p>`,
+      });
+      res.status(200).json({
+        message: "Password reset link send to your email.",
+      });
+    } catch (emailError) {
+      console.error("Error sending email:", emailError);
+      return res
+        .status(500)
+        .json({ message: "Failed to send email. Please try again." });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { signUp, signIn, forgotPassword };
--- BE/src/routes/userRoute.js
+++ BE/src/routes/userRoute.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { signUp, signIn } = require("../controllers/userControllers");
+const {
+  signUp,
+  signIn,
+  forgotPassword,
+} = require("../controllers/userControllers");
 const userRouter = express.Router();
 
 userRouter.post("/signup", signUp);
 
 userRouter.post("/signin", signIn);
+
+userRouter.post("/forgot-password", forgotPassword);
 
 module.exports = userRouter;
